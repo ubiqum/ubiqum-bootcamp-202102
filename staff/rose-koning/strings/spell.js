711ada798dabd185ce5699afe35ae98faa72056f
RoseKoning <rosekoning19@gmail.com>
2021-01-28 10:05:29 +0100

    Add finished spell challenge

--- staff/rose-koning/strings/spell.js
+++ staff/rose-koning/strings/spell.js
@@ -1,1 +1,9 @@
-console.log('hello world!');
+function spell(text){
+ var str = text;
+for (var i = 0; i < str.length; i++) {
+ console.log(str.charAt(i));
+}}
+
+spell("does this work?");
+
+spell("yes it does!")
