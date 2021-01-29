7f1c6cb4baca22b1c780fc183276ea023e9bb001
RoseKoning <rosekoning19@gmail.com>
2021-01-29 10:56:33 +0100

    update search.js

--- staff/rose-koning/strings/search.js
+++ staff/rose-koning/strings/search.js
@@ -1,7 +1,9 @@
 function search(text,target){
-    console.log(text.indexOf(target));
+    var lowTarget =(target.toLowerCase());
+    var lowText =(text.toLowerCase());
+    console.log(lowText.indexOf(lowTarget));
 }
 
 search("hello world this is rose", "rose");
 search("This is a longer test to see if it is really working","if");
-search("hello world this is rose","missing");
+search("hello world this is rose","WORLD");
