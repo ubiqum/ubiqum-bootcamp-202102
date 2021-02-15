3dc41813307f4c41e6a26ed0713a8ccc5f17f47c
davide-st <davide.stefani@hotmail.com>
2021-02-15 13:08:45 +0100

    Create new branch and start exercize in Javascript

---
+++ staff/davide-stefani/course-2/playground/main.js
@@ -0,0 +1,14 @@
+var myName = 'Davide'
+console.log(myName);
+var age = 34
+console.log(age);
+var ignasiAge = 32
+var ageDiff = age-ignasiAge
+console.log(ageDiff);
+function ageMessage(age){
+    if (age>21){
+        return("you are older then 21"); 
+    } else {
+        return("you are not older then 21");
+    }
+}
