f257215f8205918899d90d45d7c4517dbb956ad8
RoseKoning <rosekoning19@gmail.com>
2021-02-16 15:08:21 +0100

    add finished split.js

--- staff/rose-koning/strings/concat.js
+++
@@ -1,7 +0,0 @@
-function concat(...allArgs){
-
-console.log(allArgs.join(''));
-}
-
-concat("does","this","work","?");
-concat("yes","it", "does", "!");
--- staff/rose-koning/strings/count-words.js
+++
@@ -1,7 +0,0 @@
-function countWords(text){
-    var spaceCount = (text.split(" ").length - 1);
-    var wordCount = spaceCount+1;
-    console.log(wordCount);
-}
-countWords("is this working?");
-countWords("yes this is really working look at this!");
