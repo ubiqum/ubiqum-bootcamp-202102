8866d5ec647e9a65a16edea0a30309d0dafc9f47
RoseKoning <rosekoning19@gmail.com>
2021-02-16 15:11:19 +0100

    finished split.js

--- staff/rose-koning/project-2/strings/split.js
+++ staff/rose-koning/project-2/strings/split.js
@@ -1,15 +1,20 @@
 function split(text, pattern){
-
-var tempArray;
-for( var i=0; i<text.length ; i++){
-    var splitPoint = text.indexOf(pattern);
-    if (tempArray.length ===0){
-        tempArray.push(text.substring(0,splitpoint))
+    debugger
+    var tempArray = [];
+    tempArray.push(text.substring(0,text.indexOf(pattern)));
+    var addCurrentString =(text.substring((text.indexOf(pattern))+1));
+    
+    for( var i=0; i<addCurrentString.length ; i++){
+        if(i=== addCurrentString.indexOf(pattern)){
+          
+            tempArray.push(addCurrentString.substring(0,addCurrentString.indexOf(pattern)));
+            var updatedString = (addCurrentString.substring(addCurrentString.indexOf(pattern)+1))
+            tempArray.push(updatedString);
+            addCurrentString=updatedString;
+        }
+        }
+        console.log(tempArray)
     }
-    if(splitPoint!==" "){
-        tempArray.push(text.substring(i,splitPoint));
-    }
-}
-
-}
-split("hello world"," ");
+    
+    
+    split("hello world","o");
