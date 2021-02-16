6bbb98ea111d72b27f5df19489ad326abde422d3
RoseKoning <rosekoning19@gmail.com>
2021-02-16 12:20:49 +0100

    add longest-word and first-letter

--- staff/rose-koning/project-2/playground/first-letter-capital.js
+++ staff/rose-koning/project-2/playground/first-letter-capital.js
@@ -1,20 +1,9 @@
-function firstCapital(x){
-    debugger
-    var firstLetter = (x.charAt(0));
-    var capFirstLetter =(firstLetter.toUpperCase());
-    var stringLeft = (x.slice(1));
-    
-
-    for(var i=0; i<x.length ; i++){
-        if(i===" "){
-            var firstLetterWord = (x.charAt(i));
-            var capLetter =(firstLetterWord.toUpperCase());
-            var restSentence =(x.slice((i+1)));
-            var results = capLetter+restSentence;
-    
-        }
-        var print = stringLeft+results;
-    }
+function firstCapital(string){
+ var tempArray = string.split(" ");
+for(var i=0; i< tempArray.length; i++){
+    tempArray[i]= ((tempArray[i].charAt(0))).toUpperCase() +(tempArray[i].slice(1));
+}
+console.log(tempArray .join(` `));
 }
 x = "prince of persia";
 firstCapital(x);
