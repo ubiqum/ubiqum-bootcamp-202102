ff86b8991da0187685a648fc5a90cfa103b87a45
RoseKoning <rosekoning19@gmail.com>
2021-02-17 09:33:45 +0100

    add replace

--- staff/rose-koning/project-2/strings/indexof.js
+++
@@ -1,8 +0,0 @@
-function indexOf(text, pattern) {
-  for (i = 0; (text.charAt(i))!==null; i++) {
-    if((text.charAt(i))===pattern){
-        console.log(i)
-    }
-  }
-}
-indexOf("is this working?", "t");
