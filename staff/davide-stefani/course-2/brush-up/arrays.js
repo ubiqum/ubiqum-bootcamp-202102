86d16ddf9d5a61c08153a14bcea8a97c49ba6ceb
davide-st <davide.stefani@hotmail.com>
2021-02-17 11:58:30 +0100

    add exercise arrays

---
+++ staff/davide-stefani/course-2/brush-up/arrays.js
@@ -0,0 +1,16 @@
+//exercise1:
+var classmates=["Rose","Puja","Enric","Luca","Davide","Manuel"];
+classmates.sort();
+console.log(classmates[0]);
+console.log(classmates[5]);
+for (i=0; i<classmates.length; i++){
+    console.log(classmates[i]);
+};
+//exercise2:
+var ages=[34,30,37,29,31,25];
+while (ages<ages.length) {console.log(ages);
+};
+
+
+
+
--- staff/davide-stefani/course-2/brush-up/basics.js
+++ staff/davide-stefani/course-2/brush-up/basics.js
@@ -1,15 +1,20 @@
+//exercise1:
 var myName = 'Davide'
 console.log(myName);
+//exercise2:
 var age = 34
 console.log(age);
+//exercize3:
 var ignasiAge = 32
 var ageDiff = age-ignasiAge
 console.log(ageDiff);
+//exercise4:
 if (age>21){
     console.log("you are older then 21"); 
 } else {
     console.log("you are not older then 21");
 };
+//exercise5:
 if(age<ignasiAge){
     console.log("Ignasi is older then you");
 } else if (age>ignasiAge){
