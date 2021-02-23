fd9bce95abaf51741e630a68df522386b65741a6
RoseKoning <rosekoning19@gmail.com>
2021-02-23 16:26:50 +0100

    add tgif

---
+++ staff/rose-koning/project-2/TGIF/add-data-to-pre.js
@@ -0,0 +1,10 @@
+
+var resultTable = document.querySelector("resultTable")
+
+function makeTable(){
+    for(i=0; i< 10; i++){
+        resultTable.insertRow(i);
+     
+    }
+    }
+makeTable(resultTable);
