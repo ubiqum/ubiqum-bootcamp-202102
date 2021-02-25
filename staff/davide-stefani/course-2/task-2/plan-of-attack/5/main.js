eae7a3d10517c1ea74e96f2b3271ccb88debdad8
davide-st <davide.stefani@hotmail.com>
2021-02-25 11:47:44 +0100

    Add links and header in folder 6

--- staff/davide-stefani/course-2/task-2/plan-of-attack/5/main.js
+++ staff/davide-stefani/course-2/task-2/plan-of-attack/5/main.js
@@ -1,28 +1,28 @@
 var table = document.getElementById("senate-data");
 
-var members=data.results[0].members;
+var members = data.results[0].members;
 
 
-members.forEach(function(member){
-var row = document.createElement('tr');
+members.forEach(function (member) {
+    var row = document.createElement('tr');
 
-var fullname = document.createElement('td');
-fullname.innerText = (member.first_name)+' '+(member.middle_name ||='')+' '+(member.last_name);
+    var fullname = document.createElement('td');
+    fullname.innerText = (member.first_name) + ' ' + (member.middle_name ||= '') + ' ' + (member.last_name);
 
-var party = document.createElement('td');
-party.innerText = member.party;
+    var party = document.createElement('td');
+    party.innerText = member.party;
 
-var state = document.createElement('td');
-state.innerText = member.state;
+    var state = document.createElement('td');
+    state.innerText = member.state;
 
-var seniority = document.createElement('td');
-seniority.innerText = member.seniority;
+    var seniority = document.createElement('td');
+    seniority.innerText = member.seniority;
 
-var votepercentage = document.createElement('td');
-votepercentage.innerText = member.votes_with_party_pct +" %";
+    var votepercentage = document.createElement('td');
+    votepercentage.innerText = member.votes_with_party_pct + " %";
 
-row.append (fullname, party, state, seniority, votepercentage)
+    row.append(fullname, party, state, seniority, votepercentage)
 
-table.append (row)
+    table.append(row)
 
 })
