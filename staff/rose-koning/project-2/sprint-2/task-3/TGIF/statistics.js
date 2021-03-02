ac34a8378ab42c5d5f0c51a7069bb25070fb3ce6
RoseKoning <rosekoning19@gmail.com>
2021-03-02 12:48:42 +0100

    add updated code in statistics.js

--- staff/rose-koning/project-2/sprint-2/task-3/TGIF/statistics.js
+++ staff/rose-koning/project-2/sprint-2/task-3/TGIF/statistics.js
@@ -10,111 +10,73 @@ statistics.numberOfDemocrats= group.D.length;
 statistics.numberOfIndependents = group.ID.length;
 
 // republican votes compared to democrat votes
-var partyVotes = [];
-var individualVotesR = []
-var individualVotesD = []
+var votes = {R:[],D:[],ID:[]};
+var averages ={};
 for (var party in group) {
   var senators = group[party];
   for (var senator of senators) {
-    if(party==="R"){
-      var senatorVotesR = senator.votes_with_party_pct;
-      individualVotesR.push(senatorVotesR)}
-    if(party==="D"){
-      var senatorVotesD = senator.votes_with_party_pct;
-      individualVotesD.push(senatorVotesD)}
+    votes[party].push(senator.votes_with_party_pct);
+    
+    averages[party]= ((votes[party].reduce((a, b) => a + b, 0))/votes[party].length).toFixed(2)
   
-   var averageVotesR = ((individualVotesR.reduce((a, b) => a + b, 0))/statistics.numberOfRepublicans).toFixed(2);
-   var averageVotesD = ((individualVotesD.reduce((a, b) => a + b, 0))/statistics.numberOfDemocrats).toFixed(2);
-   var repComparedtoDem = ((averageVotesR-averageVotesD)/averageVotesD)*100;
+}
+var repComparedtoDem = (((averages.R-averages.D)/averages.D)*100).toFixed(2)
+;
+statistics.democratVSRepublicanVotes= repComparedtoDem;
 }
 
-
-
-}
-// var totalRepublicanVotes = [];
-//   for (var i = 0; i < stat; i++) {
-//     totalRepublicanVotes.push(republicanMembers[i].votes_with_party_pct);
-//   }
+  //lowest 10% votes
+  var lowestVotes={R:[],D:[],ID:[]};
+  var senatorsOrdered=[];
+  for (var party in group) {
+    var senators = group[party];
+    var tenPercent = Math.ceil((senators.length/100)*10);
+    senatorsOrdered.push(senators.sort((a, b) => {return a.votes_with_party_pct - b.votes_with_party_pct;}));
+        for (i=0; i< tenPercent; i++){
+          lowestVotes[party].push(senators[i])}
+        }
   
-  
-//   var averageRepVote = ((totalRepublicanVotes.reduce((a, b) => a + b, 0))/statistics.numberOfRepublicans);
+  statistics.lowestVotesWithRepublicans=lowestVotes.R;
+  statistics.lowestVotesWithDemocrats=lowestVotes.D;
 
-//   var totalDemocratVotes = [];
-//   for (var i = 0; i < democratMembers.length; i++) {
-//     totalDemocratVotes.push(democratMembers[i].votes_with_party_pct);
-//   }
-//   var averageDemVote = ((totalDemocratVotes.reduce((a, b) => a + b, 0))/statistics.numberOfDemocrats);
+   //highest 10% votes
+   var highestVotes={R:[],D:[],ID:[]};
+   var senatorsOrdered=[];
+   for (var party in group) {
+     var senators = group[party];
+     var tenPercent = Math.ceil((senators.length/100)*10);
+     senatorsOrdered.push(senators.sort((a, b) => {return b.votes_with_party_pct - a.votes_with_party_pct;}));
+         for (i=0; i< tenPercent; i++){
+           highestVotes[party].push(senators[i])}
+         }
+   
+   statistics.highestVotesWithRepublicans=highestVotes.R;
+   statistics.highestVotesWithDemocrats=highestVotes.D;
 
-//   var repComparedToDem = ((averageRepVote-averageDemVote)/averageDemVote)*100
+//10% least missed votes
+var leastVotes={R:[],D:[],ID:[]};
+var senatorsOrdered=[];
+for (var party in group) {
+  var senators = group[party];
+  var tenPercent = Math.ceil((senators.length/100)*10);
+  senatorsOrdered.push(senators.sort((a, b) => {return a.missed_votes_pct - b.missed_votes_pct;}));
+      for (i=0; i< tenPercent; i++){
+        leastVotes[party].push(senators[i])}
+      }
 
-//   statistics.democratVSRepublicanVotes = repComparedToDem.toFixed(2);
+statistics.leastVotesMissedRepublican=leastVotes.R;
+statistics.leastVotesMissedDemocrat=leastVotes.D;
 
-  //lowest 10% votes republicans
-//   totalRepublicanVotes.sort();
-//   r10 = (statistics.numberOfRepublicans/100)*10;
-//   lowest10RMembers = [];
-//   for (i=0; i< r10; i++){
-// lowest10RMembers.push(republicanMembers[i])
-//   }
-//   statistics.lowestVotesWithRepublicans=lowest10RMembers;
+//10% most missed votes
+var mostVotes={R:[],D:[],ID:[]};
+var senatorsOrdered=[];
+for (var party in group) {
+  var senators = group[party];
+  var tenPercent = Math.ceil((senators.length/100)*10);
+  senatorsOrdered.push(senators.sort((a, b) => {return b.missed_votes_pct - a.missed_votes_pct;}));
+      for (i=0; i< tenPercent; i++){
+        mostVotes[party].push(senators[i])}
+      }
 
-//   //lowest 10% votes democrats
-//   totalDemocratVotes.sort();
-//   d10 = (statistics.numberOfDemocrats/100)*10;
-//   lowest10DMembers = [];
-//   for (i=0; i< d10; i++){
-// lowest10DMembers.push(democratMembers[i])
-//   }
-//   statistics.lowestVotesWithDemocrats=lowest10DMembers;
-
-//   //highest 10% votes republicans
-//   totalRepublicanVotes.sort(function(a, b){return b-a}); 
-//   highest10RMembers = [];
-//   for (i=0; i< r10; i++){
-// highest10RMembers.push(republicanMembers[i])
-//   }
-//   statistics.highVotesWithRepublicans=highest10RMembers;
-
-//   //highest 10% votes republicans
-//   totalDemocratVotes.sort(function(a, b){return b-a}); 
-//   highest10DMembers = [];
-//   for (i=0; i< d10; i++){
-// highest10DMembers.push(democratMembers[i])
-//   }
-//   statistics.highVotesWithDemocrats=highest10DMembers;
-
-//   //least votes missed democrats
-//   var sortedDemocratMembers = democratMembers.sort((a, b) => (a.missed_votes > b.missed_votes) ? 1 : -1)
-//   var lowestVotesMissedD=[];
-//   for(var i = 0; i<d10 ; i++){
-//   lowestVotesMissedD.push(sortedDemocratMembers[i]);
-//   }
-
-//   statistics.leastVotesMissedDemocrat =lowestVotesMissedD;
-
-//    //least votes missed republican
-//    var sortedRepublicanMembers = republicanMembers.sort((a, b) => (a.missed_votes > b.missed_votes) ? 1 : -1)
-//    var lowestVotesMissedR=[];
-//    for(var i = 0; i<r10 ; i++){
-//    lowestVotesMissedR.push(sortedRepublicanMembers[i]);
-//    }
- 
-//    statistics.leastVotesMissedRepublican =lowestVotesMissedR;
-
-//     //most votes missed democrats
-//     var sortedDemocratMembers = democratMembers.sort((a, b) => (a.missed_votes > b.missed_votes) ? -1 : 1)
-//     var mostVotesMissedD=[];
-//     for(var i = 0; i<d10 ; i++){
-//     mostVotesMissedD.push(sortedDemocratMembers[i]);
-//     }
-  
-//     statistics.mostVotesMissedDemocrat =mostVotesMissedD;
-
-//     //most votes missed republicans
-//     var sortedRepublicanMembers = republicanMembers.sort((a, b) => (a.missed_votes > b.missed_votes) ? -1 : 1)
-//     var mostVotesMissedR=[];
-//     for(var i = 0; i<r10 ; i++){
-//     mostVotesMissedR.push(sortedRepublicanMembers[i]);
-//     }
-  
-//     statistics.mostVotesMissedRepublican =mostVotesMissedR;
+statistics.mostVotesMissedRepublican=mostVotes.R;
+statistics.mostVotesMissedDemocrat=mostVotes.D;
