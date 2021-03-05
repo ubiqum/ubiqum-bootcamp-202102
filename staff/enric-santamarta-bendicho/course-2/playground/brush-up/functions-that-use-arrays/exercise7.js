f63433e1051bcbc6eae00174e3f2858f6e8eec4b
death4santamarta <enricsantamarta669@googlemail.com>
2021-03-05 07:37:23 +0100

    array pushed to string

---
+++ staff/enric-santamarta-bendicho/course-2/playground/brush-up/functions-that-use-arrays/exercise7.js
@@ -0,0 +1,11 @@
+
+myColor = ["Red", "Green", "White", "Black"];
+
+function Color(array) {
+    colorstring = "";
+    colorstring = myColor.join(" " )
+  /*  for (i = 0; i <= array.length; i++) { colorstring = сolorstring + array[i] + " " } */
+   console.log(colorstring)
+}
+
+Color(myColor)
