453c79df1ffe27fe5c7aaa335f5d978a7d61074b
death4santamarta <enricsantamarta669@googlemail.com>
2021-03-05 14:36:46 +0100

    string spell added

--- staff/enric-santamarta-bendicho/strings/spell.js
+++ staff/enric-santamarta-bendicho/strings/spell.js
@@ -1,9 +1,7 @@
-debugger
+
 function spell(text) {
-    for (i=0;i<text.length;i++)
-    {   if (text.length <= i)
-        { break;}
-        console.log(text[i])}
+    for (i = 0; i < text.length; i++)
+        console.log(text[i])
 }
 
 spell("Hola Mundo")
