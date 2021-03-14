1e7a9ff09184c721c29661b8ef1f21b57dca7bc9
davide-st <davide.stefani@hotmail.com>
2021-03-14 19:39:21 +0100

    Add Least Voted

--- staff/davide-stefani/course-2/task-3/plan-of-attack/4/statistics.js
+++ staff/davide-stefani/course-2/task-3/plan-of-attack/4/statistics.js
@@ -33,8 +33,33 @@ var sum_votes_Dem = democrats.reduce(function (accumulator, currentValue) {
 },initialValue);
 var average_votes_Dem=sum_votes_Dem/democrats.length;
 
+//make it a comment when calculate house as No indipendents
+/*
 var sum_votes_Ind = indipendents.reduce(function (accumulator, currentValue) {
     return accumulator + currentValue.votes_with_party_pct;
 },initialValue);
 var average_votes_Ind=sum_votes_Ind/indipendents.length;
+*/
 
+// Identyfy Members who least votes with their Parties
+
+members.sort(function(a, b) {
+    return a.votes_with_party_pct - b.votes_with_party_pct
+})
+
+var leastOftenVoteMembers = []
+
+var limit = Math.round(members.length / 10)
+
+var loop = true
+
+for (var i = 0; loop; i++) {
+    var member = members[i]
+
+    if (i < limit) {
+        leastOftenVoteMembers.push(member)
+    } else if (leastOftenVoteMembers[limit - 1].votes_with_party_pct === member.votes_with_party_pct) {
+        leastOftenVoteMembers.push(member)
+    } else loop = false
+}
+false
