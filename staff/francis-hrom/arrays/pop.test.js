7c3d509d66129135926254619dad3e493e10b518
francis-hrom <74234592+francis-hrom@users.noreply.github.com>
2021-03-25 17:09:37 +0100

    pop.js

---
+++ staff/francis-hrom/arrays/pop.test.js
@@ -0,0 +1,4 @@
+var arr = ["Banana", "Orange"];
+console.assert(pop(arr)==="Orange","pop(arr)==='Orange'");
+console.assert(arr.length===1,"Array length is 1.");
+console.assert(JSON.stringify(arr)===JSON.stringify(["Banana"]),'Array is in format ["Banana"]');
