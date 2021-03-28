9255f438dffd875e4dc3bfac8bda787e49b0e252
francis-hrom <74234592+francis-hrom@users.noreply.github.com>
2021-03-28 12:58:57 +0200

    includes.js

---
+++ staff/francis-hrom/arrays/includes.test.js
@@ -0,0 +1,4 @@
+var arr = ["Kiwi", "Mango", "Apple"];
+console.assert(includes(arr,"Mango")===true,"Array includes 'Mango'");
+console.assert(includes(arr,"Samuro")===false,"Array does not include 'Samuro'");
+console.assert(arr.length===3,"Array length is 3.");
