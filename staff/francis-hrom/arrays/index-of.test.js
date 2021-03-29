9f933184ca6fbe019afea6d0f38cb91dd9e013d5
francis-hrom <74234592+francis-hrom@users.noreply.github.com>
2021-03-29 13:44:07 +0200

    index-of.js

---
+++ staff/francis-hrom/arrays/index-of.test.js
@@ -0,0 +1,4 @@
+var arr = ["Kiwi", "Mango", "Apple"];
+console.assert(indexOf(arr,"Mango")===1,"Array includes 'Mango' at index 1");
+console.assert(indexOf(arr,"Samuro")===-1,"Array does not include 'Samuro'");
+console.assert(arr.length===3,"Array length is 3.");
