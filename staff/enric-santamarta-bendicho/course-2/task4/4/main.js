ccbb4a376be0ed175bb5d2b54b5adc094e602b1d
death4santamarta <enricsantamarta669@googlemail.com>
2021-04-09 07:32:52 +0200

    add filter Members and retrieveMember callback

--- staff/enric-santamarta-bendicho/course-2/task4/4/main.js
+++ staff/enric-santamarta-bendicho/course-2/task4/4/main.js
@@ -1,43 +1,21 @@
-retrieveMembers(function (members) {
-    app.members = members
-});
-
-
 var app = new Vue({
-    el: '#app',
-    data: {
-        members: {},
-        parties:[],
-        state:null
-    },
-    methods:{
-        fillTable: function(){
-            this.members = {}},
-        applyFilters:function(){
-            this.members = filterMembers(this.parties,this.state,this);
-        }
+  el: '#app',
+  data: {
+    filteredMembers: [],
+    parties: [],
+    state: null
+  },
+  methods: {
+    applyFilters: function () {
+       filterMembers(this.parties, this.state, function(filteredMembers){
+        this.filteredMembers = filteredMembers  
+      }.bind(this));
     }
+  },
+  created: function () {
+    retrieveMembers(function (members) {
+      app.members = members
+      app.filteredMembers = members
+    });
+  }
 })
-
-
- function filterMembers(parties,state) {
-  var members = app.members
-    if (parties.length) {
-      members = members.filter(
-        function (member) {
-          return parties.includes(member.party);
-        }
-      );
-    }
-    if (state) {
-        members = members.filter(
-          function (member) {
-            return member.state === state;
-          }
-        );
-      }
-    
-      return members;
-    }
-
-
