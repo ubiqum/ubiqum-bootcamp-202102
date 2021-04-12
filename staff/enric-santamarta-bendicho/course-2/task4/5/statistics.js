060eb84e9ed92c575298c8881ef1cfd5620b2045
death4santamarta <enricsantamarta669@googlemail.com>
2021-04-12 16:57:34 +0200

    add vue statistics

--- staff/enric-santamarta-bendicho/course-2/task4/5/statistics.js
+++ staff/enric-santamarta-bendicho/course-2/task4/5/statistics.js
@@ -1,21 +1,3 @@
-function retrieveMembers(callback) {
-    fetch("https://api.propublica.org/congress/v1/113/senate/members.json",
-        {
-            headers: {
-                "X-API-Key": "I0REsGy5W1Niu7TP6VKCAF8dVD68NILvYaucpFhY"
-            }
-        }
-    )
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data) {
-            callback(data.results[0].members);
-
-        })
-}
-
-
 //collect Data 
 retrieveMembers(function (members) {
     var members = members
