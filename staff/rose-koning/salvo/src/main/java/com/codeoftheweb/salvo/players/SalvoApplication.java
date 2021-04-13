258b9fcdc337870fe3c40aa8f26dbff5ac135965
RoseKoning <rosekoning19@gmail.com>
2021-04-13 12:31:18 +0200

    add working html and js

---
+++ staff/rose-koning/salvo/src/main/java/com/codeoftheweb/salvo/players/SalvoApplication.java
@@ -0,0 +1,26 @@
+package com.codeoftheweb.salvo.players;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
+
+@SpringBootApplication
+public class SalvoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalvoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initData(PlayerRepository repository) {
+		return (args) -> {
+			// save a couple of customers
+			repository.save(new Player("Jack"));
+			repository.save(new Player("Chloe"));
+			repository.save(new Player("Kim"));
+			repository.save(new Player("David"));
+			repository.save(new Player("Michelle"));
+		};
+	}
+}
