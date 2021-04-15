3d6d90a16aa319f7cd9c5bb0ccaabd94b30ede9e
RoseKoning <rosekoning19@gmail.com>
2021-04-15 09:26:50 +0200

    add salvo controller

---
+++ staff/rose-koning/salvo/src/main/java/com/codeoftheweb/salvo/SalvoController.java
@@ -0,0 +1,18 @@
+package com.codeoftheweb.salvo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+public class SalvoController {
+
+    @Autowired
+    private GameRepository repository;
+
+    @RequestMapping("/api")
+    public List<Game> getAll() {
+        return repository.findAll();
+    }
+}
