8c5bd0ef72f919c40d299a57a1b083111d6b162c
francis-hrom <74234592+francis-hrom@users.noreply.github.com>
2021-04-20 07:13:02 +0200

    3.1. Create Senate HTML File and JSON Statistics Object

---
+++ staff/francis-hrom/course-2/3.1/statistics.js
@@ -0,0 +1,14 @@
+const senateStatistics = {
+    "number_of_democracts": 0,
+    "number_of_republicans": 0,
+    "number_of_independents": 0,
+    "total_number_of_representatives": 0,
+    "number_of_democracts_voted_with_party": 0,
+    "number_of_republicans_voted_with_party": 0,
+    "number_of_independents_voted_with_party": 0,
+    "total_number_of_representatives_voted_with_party": 0,
+    "least_engaged_threshold": 0,
+    "most_engaged_threshold": 0,
+    "least_loyal_threshold": 0,
+    "most_loyal_threshold": 0
+};
