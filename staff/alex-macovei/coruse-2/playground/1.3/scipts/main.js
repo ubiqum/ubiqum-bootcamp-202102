6bdec106f246fc97e5ef30528aac64df6eb22e86
alex_maco34 <alex_maco34@libero.it>
2021-04-22 12:05:22 +0200

    finished 1.3, added 1.4 and also added comments to every function

--- staff/alex-macovei/coruse-2/playground/1.3/scipts/main.js
+++ staff/alex-macovei/coruse-2/playground/1.3/scipts/main.js
@@ -1,8 +1,10 @@
  console.log("Starting javascript...");
 var ages = [27,29,22,24];
 var b = 0;
-var simpleArray = [22,45,74,23,12,99,81,45,21,22];
+var simpleArray = [22,45,74,23,12,99,81,45,21,22,99,12];
 var names = ["Sebastian","Francis","Max","Alex"];
+var stringArray = ["hello","my","name", "is","Alex"]
+
 
 names.sort();
 console.log(names[0]);
@@ -22,7 +24,7 @@ for (var i=0;i<ages.length;i++){
    }
 }
 
-function lowestNumber (array){
+function lowestNumber (array){ //task 3 prints the lowest number in the specified array
    var num = array[0];
    for(var i=0;i<array.length;i++){
       if(num>array[i]){
@@ -34,7 +36,7 @@ function lowestNumber (array){
 
 console.log(lowestNumber(simpleArray));
 
-function biggestNumber (array){
+function biggestNumber (array){ //task 4 prints the biggest number in the array specified
    var num = array[0];
    for(var i=0;i<array.length;i++){
       if(num<array[i]){
@@ -46,20 +48,34 @@ function biggestNumber (array){
 
 console.log(biggestNumber(simpleArray));
 
-function printIndex (array, index){
+function printIndex (array, index){ //task 5, checking array and returning the specified number position from the array
    return array[index];
 }
 
 console.log(printIndex(simpleArray,3));
 
-function doubles(array){
+function doubles(array){ //Task 6, checking array and returning doubles in the array
+   var returnArray = [];
+   var c = 0;
    for(var i=0;i<array.length;i++){
       for(var b=0;b<array.length;b++){
-         if (array[i]==array[i]){
-            return array[i];
+         if(b!=i && array[i]==array[b] && !returnArray.includes(array[i])){
+            returnArray[c] = array[i];
+            c++;
          }
       }
    }
+   return returnArray;
 }
 
 console.log(doubles(simpleArray));
+
+function combine(array) { //Task 7, takes array and transforms it into a string with quotes and spaces in between
+   var returnString = "";
+   for (var i=0;i<array.length;i++){
+      returnString = returnString+"\""+array[i]+"\" ";
+   }
+   return returnString;
+}
+
+console.log(combine(stringArray))
---
+++ staff/alex-macovei/coruse-2/playground/1.4/scipts/main.js
@@ -0,0 +1,23 @@
+var normalString = "123456789"
+var normalString1 = "Macovei"
+
+function reverseString(string){ //Task 1, takes string and reverses it
+   var returnString = "";
+   for(var i=0;i<string.length+1;i++){
+      returnString = returnString+string.charAt([string.length-i]);
+   }
+   return returnString;
+}
+
+console.log(reverseString(normalString));
+
+function order(string){
+   string = string.toLowerCase();
+   var returnString = "";
+   
+   for (var i=0;i<string.length;i++){
+      for (var b=0;b<string.length;b++){
+         
+      }
+   }
+}
