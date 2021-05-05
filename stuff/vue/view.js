9d6362b774fc19255323dd39e34dcbb3c4ba4950
manuelbarzi <manuelbarzi@gmail.com>
2021-05-05 12:47:10 +0200

    boost vue demo

--- stuff/vue/view.js
+++ stuff/vue/view.js
@@ -1,38 +1,93 @@
-const Login = {
+const Home = {
     template: `<main>
-        <h1>Login</h1>
-        <form v-on:submit="search" method="post">
-            <input type="text" name="username">
-            <input type="password" name="password">
-            <button>Submit</button>
-        </form>
+        Welcome to the Music Events App!
+        <p>lorem ipsum.... </p>
+    </main>`
+}
+
+const Events = {
+    template: `<main>
+        <h1>Events</h1>
+
+        <div v-if="events">
+            <ul v-if="events.length">
+                <li v-for="event in events" :key="event.id">
+                    <h2>{{ event.title }}</h2>
+                    <p>{{ event.location }}, <time>{{ event.datetime }}</time></p>
+                    <ul v-if="event.tags">
+                        <li v-for="tag in event.tags" :key="tag">
+                            {{ tag }}
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <p v-else>no events :/</p>
+        </div>
     </main>`,
-    methods: {
-        search(event) {
-            event.preventDefault()
-
-            var username = event.target.username.value
-            var password = event.target.password.value
-
-            if (username === 'pepito' && password === 'grillo')    
-                router.push('/home')
+    data() {
+        return {
+            events: null
         }
+    },
+    created() {
+        retrieveEvents(function (error, events) {
+            if (error) return alert(error.message)
+
+            this.events = events
+        }.bind(this))
     }
 }
 
-const Home = {
-    template: '<h1>hola mundo</h1>'
+const Participants = {
+    template: `<main>
+        <h1>Participants</h1>
+
+        <div v-if="participants">
+            <ul v-if="participants.length">
+                <li v-for="participant in participants" :key="participant.id">
+                    <h2>{{ participant.name }}</h2>
+
+                    <ul v-if="participant.tags">
+                        <li v-for="tag in participant.tags" :key="tag">
+                            {{ tag }}
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+            <p v-else>no participants :/</p>
+        </div>
+    </main>`,
+    data() {
+        return {
+            participants: null
+        }
+    },
+    created() {
+        retrieveParticipants(function (error, participants) {
+            if (error) return alert(error.message)
+
+            this.participants = participants
+        }.bind(this))
+    }
 }
 
 const router = new VueRouter({
     routes: [
-        { path: '/login', component: Login },
-        { path: '/home', component: Home }
+        { path: '/', component: Home },
+        { path: '/events', component: Events },
+        { path: '/participants', component: Participants }
     ]
 })
 
 const App = {
     el: '#app',
     router,
-    template: '<router-view></router-view>'
+    template: `<div>
+    <nav>
+        <router-link to="/">Home</router-link>
+        <router-link to="/events">Events</router-link>
+        <router-link to="/participants">Participants</router-link>
+    </nav>
+    <router-view></router-view>
+</div>`
 }
