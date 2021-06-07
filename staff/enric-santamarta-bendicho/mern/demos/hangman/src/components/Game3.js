67a54a6777fca3bc1664a208ff2cab65d181b89a
death4santamarta <enricsantamarta669@googlemail.com>
2021-06-07 18:42:27 +0200

    add hangman with improved logic

--- staff/enric-santamarta-bendicho/mern/demos/hangman/src/components/Game3.js
+++ staff/enric-santamarta-bendicho/mern/demos/hangman/src/components/Game3.js
@@ -28,61 +28,66 @@ class Game extends Component {
     handleSubmitGuess(charOrWord) {
         // TODO check char or word. if char check and show it in guessed array. if word check if matches the word in state.
         //const attemps = this.state.attemps
-        // TODO learn js destructuring
-
+        const { state: { attemps, correctAttemps } } = this // TODO learn js destructuring
+      
 
         if (charOrWord.length === 1) {
-            this.handleGuessedCharacter(charOrWord)
+            this.handleGuessedCharacter(charOrWord, attemps, correctAttemps)
         } else {
-            this.handleGuessedWord(charOrWord)
+            this.handleGuessedWord(charOrWord, attemps)
         }
 
     }
 
 
-    handleGuessedCharacter(charOrWord) { // use from state
-        let { state: { word, guessed, attemps, correctAttemps } } = this
+    handleGuessedCharacter(charOrWord, attemps, correctAttemps) { // use from state
+        let { state: { word, guessed } } = this
 
         guessed = [...guessed]
 
         for (var i = 0; i <= word.length; i++) {
             if (charOrWord === word[i]) {
-
                 guessed[i] = charOrWord
-
                 correctAttemps++
 
-                
+                // this.setState({ correctAttemps: correctAttemps + 1 }, () => { this.handleCorrectAttemps() })
+
+                // this.setState({ attemps: attemps + 1 }, () => { this.handleMaxAttemps() })
+
+                // this.handleSubmitShowedLetters(i, charOrWord)
+
             }
         }
-        attemps++
-        
-      
+
         // TODO check lose or win here
 
-        this.setState({ guessed, attemps, correctAttemps }, () => { this.checkWinorLose() })
+        this.setState({ guessed, attemps: ++attemps, correctAttemps }, () => { this.handleMaxAttemps() })
     }
 
+    handleSubmitShowedLetters(i, charOrWord) {
 
-    handleGuessedWord(charOrWord) {
-        let { state: { word, attemps, correctAttemps,guessed } } = this
+      // if(this.state.guessed[i] === ('-')){ 
+        const guessed2 = this.state.guessed.slice()
 
-        if (charOrWord !== word) {
+        guessed2[i] = charOrWord 
 
-            attemps++
+        this.setState({ guessed: guessed2 })
+       // }
 
-            this.setState({ attemps }, () => { this.checkWinorLose() })
+    }
+
+    handleGuessedWord(charOrWord, attemps) {
+
+        if (charOrWord !== this.state.word) {
+
+            this.setState({ attemps: attemps + 1 }, () => { this.handleMaxAttemps() })
 
         }
-        if (charOrWord === word) {
+        if (charOrWord === this.state.word) {
 
-            attemps++
+            this.setState({ attemps: attemps + 1 }, () => { this.handleMaxAttemps() })
 
-            correctAttemps++
-
-            guessed = charOrWord
-
-            this.setState({ attemps,correctAttemps,guessed }, () => { this.checkWinorLose() })
+            this.setState({ guessed: charOrWord })
 
             this.setState({ won: true })
 
@@ -90,16 +95,17 @@ class Game extends Component {
         }
     }
 
-    checkWinorLose() {
-
-        if (this.state.attemps < MAX_ATTEMPS && this.state.correctAttemps === this.state.word.length) {
-            this.setState({ won: true })
-        }
+    handleMaxAttemps() {
         if (this.state.attemps === MAX_ATTEMPS) {
             this.setState({ lose: true })
         }
     }
 
+    handleCorrectAttemps() {
+        if (this.state.correctAttemps === this.state.word.length) {
+            this.setState({ won: true })
+        }
+    }
 
     handleSubmitRefresh() {
 
@@ -116,7 +122,7 @@ class Game extends Component {
                 {guessed}
                 <TextForm title="Guess a character or the word" onSubmit={handleSubmitGuess.bind(this)} />
                 <p>You have {MAX_ATTEMPS - attemps} tries.</p>
-                <p>You guessed {correctAttemps} Letters from {word.length}.</p>
+                <p>You guessed {correctAttemps} Letters from the secret Word.</p>
             </div>}
             {won && <div><p>you win!</p><form onSubmit={this.handleSubmitRefresh}><button>Restart the Game</button></form></div>}
             {lose && <div><p>you lose!</p><form onSubmit={this.handleSubmitRefresh}><button>Restart the Game</button></form></div>}
