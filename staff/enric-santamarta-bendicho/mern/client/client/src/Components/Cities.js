dfed3f8f08ec8ee0ac75d08b05b0b2b46e80e4c4
death4santamarta <enricsantamarta669@googlemail.com>
2021-06-21 21:05:54 +0200

    add fetch City List to client add and render

--- staff/enric-santamarta-bendicho/mern/client/client/src/Components/Cities.js
+++ staff/enric-santamarta-bendicho/mern/client/client/src/Components/Cities.js
@@ -12,26 +12,64 @@ import Button from '@material-ui/core/Button';
 import Home from './images/homeIcon.png'
 import { NavLink } from 'react-router-dom'
 
-function Cities() {
+class Cities extends Component {
+    constructor(){
+        super()
+        this.state = {
+            isFetching:false,
+            cities:[],
+            citiesFilter:""
+        }
+    }
+    componentDidMount(){
+        this.fetchCities()
+        this.timer = setInterval(() => this.fetchCities(), 5000)
+    }
+
+    componentWillUnmounte(){
+        this.timer = null
+    }
+
+    fetchCities = () => {this.setState({ ...this.state, isFetching: true })
+    fetch('http://localhost:5000/cities/all')
+        .then(response => response.json())
+        .then(result => this.setState({
+            cities: result,
+            isFetching: false}))
+        .catch(e => console.log(e))
+    }
 
 
+    render() { 
 
-    return (<div><h2>City List</h2>
+      const {state: {cities, isFetching }} = this
+      
+      const listCitiesNames = cities.map((cities) => <ul> {cities.name} </ul>)
+
+      const listCitiesCountries = cities.map((cities) => <ul> {cities.country} </ul>)
+
+    return <div><h2>City List</h2>
+        <Box>
+        <input type="text" id="filter" value={this.state.citiesFilter}
+        onChange={this.handleChange}></input>
         <TableContainer component={Paper}>
             <Table>
                 <TableHead>
-                    <TableRow><TableCell align="center">City Details</TableCell><TableCell align="center">City Routes</TableCell></TableRow>
+                    <TableRow><TableCell align="center">City</TableCell><TableCell align="center">Country</TableCell></TableRow>
                 </TableHead>
                 <TableBody>
-                    <TableRow><TableCell align="center"></TableCell><TableCell align="center"></TableCell></TableRow>
+                    <TableRow><TableCell align="center">{listCitiesNames}</TableCell><TableCell align="center">{listCitiesCountries}</TableCell></TableRow>
                 </TableBody>
             </Table>
         </TableContainer>
         <footer>
             <NavLink to='/' ><Button><img src={Home} alt="return home" /> </Button></NavLink>
         </footer>
-    </div>)
+        </Box>
+    </div>
+    }
 }
+
 
 
 export default Cities
